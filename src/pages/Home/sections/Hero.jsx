643466703fa144e05f235829425dc808253354fc
Ramez Ehab <ramezehab2@gmail.com>
2024-09-27 21:51:45 +0300

    style: adjust hero page responsivity

--- src/pages/Home/sections/Hero.jsx
+++ src/pages/Home/sections/Hero.jsx
@@ -4,25 +4,25 @@ const Hero = () => {
     return (
         <section
             id="hero"
-            className="w-full flex flex-col-reverse md:flex-row justify-center gap-10 md:gap-40 items-center py-10 px-5 md:px-10"
+            className="w-full flex flex-col-reverse sm:flex-row justify-center gap-10 md:gap-40 items-center p-5 md:p-10 my-2"
         >
-            <div>
-                <div className="flex flex-col justify-center gap-5 w-[270px] md:w-[325px]">
+            <div className="md:w-1/2">
+                <div className="flex flex-col justify-center gap-5 w-[270px] md:w-[431px]">
                     <div>
-                        <div className="flex flex-row ">
-                            <h1 className="font-rubik uppercase font-medium text-primary text-3xl md:text-4xl">Hey, I{"'"}m</h1>
+                        <div className="flex flex-row">
+                            <h1 className="font-rubik uppercase font-medium text-primary text-3xl md:text-5xl">Hey, I{"'"}m</h1>
                         </div>
                         <div className="flex flex-row justify-end">
-                            <h1 className="font-rubik uppercase font-medium text-secondary text-3xl md:text-4xl">Ramez Ehab</h1>
+                            <h1 className="font-rubik uppercase font-medium text-secondary text-3xl md:text-5xl">Ramez Ehab</h1>
                         </div>
                     </div>
 
                     <div>
                         <div className="flex flex-row">
-                            <h1 className="font-robotoMono font-medium text-primary text-lg md:text-xl">I{"'"}m a</h1>
+                            <h1 className="font-robotoMono font-medium text-primary text-lg md:text-2xl">I{"'"}m a</h1>
                         </div>
                         <div className="flex flex-row justify-end">
-                            <h1 className="font-robotoMono font-medium text-secondary text-lg md:text-xl">Mobile | Web Developer</h1>
+                            <h1 className="font-robotoMono font-medium text-secondary text-lg md:text-2xl">Mobile | Web Developer</h1>
                         </div>
                     </div>
 
@@ -39,11 +39,11 @@ const Hero = () => {
             </div>
 
 
-            <div className="">
+            <div className="w-3/4 xs:w-1/2 sm:w-1/3 md:w-1/2 lg:w-1/3">
                 <img
                     src="https://avatars.githubusercontent.com/u/117018553?v=4"
                     alt="Ramez Ehab"
-                    className="w-60 md:w-72 rounded-full"
+                    className="rounded-full"
                 />
             </div>
         </section>

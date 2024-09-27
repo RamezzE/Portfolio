902825b8ad111725c1abc5c3a7bdf7d44ebe750d
Ramez Ehab <ramezehab2@gmail.com>
2024-09-27 22:44:49 +0300

    style: made navbar stick to top

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
     return (
-      <div className="w-full flex flex-row md:gap-10 px-5 py-5 justify-around md:justify-end min-h-10">
+      <div className="w-full flex flex-row md:gap-10 px-5 py-5 justify-around md:justify-end min-h-10 sticky top-0 bg-bgColor z-50">
         <span className="font-robotoMono font-medium text-base md:text-lg text-secondary scale-110 transition transform hover:scale-110 cursor-pointer">Home</span>
         <span className="font-robotoMono font-medium text-base md:text-lg text-primary transition transform hover:scale-110 cursor-pointer">About</span>
         <span className="font-robotoMono font-medium text-base md:text-lg text-primary transition transform hover:scale-110 cursor-pointer">Projects</span>

5ae5add7df7beabfd85b0b1c00f43d25d10fa18b
Ramez Ehab <ramezehab2@gmail.com>
2024-09-28 12:56:35 +0300

    style: add framer motion & adjust styling

--- src/components/useOnScreen.jsx
+++ src/components/useOnScreen.jsx
@@ -1,16 +1,21 @@
 import { useState, useEffect, useRef } from 'react';
 
-// Hook for using intersection observer
+// Hook for using intersection observer that triggers only once
 const useOnScreen = (options) => {
   const ref = useRef(null);
   const [isIntersecting, setIntersecting] = useState(false);
+  const [hasIntersected, setHasIntersected] = useState(false); // Track if it has been intersected once
 
   useEffect(() => {
     const observer = new IntersectionObserver(([entry]) => {
-      setIntersecting(entry.isIntersecting);
+      if (entry.isIntersecting && !hasIntersected) {
+        setIntersecting(true);
+        setHasIntersected(true); // Mark as intersected
+        observer.unobserve(ref.current); // Stop observing once it has been intersected
+      }
     }, options);
 
-    if (ref.current) {
+    if (ref.current && !hasIntersected) {
       observer.observe(ref.current);
     }
 
@@ -19,7 +24,7 @@ const useOnScreen = (options) => {
         observer.unobserve(ref.current);
       }
     };
-  }, [ref, options]);
+  }, [ref, options, hasIntersected]);
 
   return [ref, isIntersecting];
 };
--- src/pages/Home/sections/Hero.jsx
+++ src/pages/Home/sections/Hero.jsx
@@ -1,57 +1,146 @@
+import { motion } from 'framer-motion';
+
 const Hero = () => {
+    // Define animation variants for reusability
+    const containerVariants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 3,
+                staggerChildren: 0.5, // Stagger animations of children
+            },
+        },
+    };
+
+    const itemVariants = {
+        hidden: { opacity: 0, y: 20 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    };
+
+    const imageVariants = {
+        hidden: { opacity: 0, scale: 0.5 },
+        visible: { opacity: 1, scale: 1, transition: { duration: 1 } },
+    };
+
+    const sideSlideVariants = {
+        hidden: { opacity: 0, x: 200 },
+        visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+    }
+
     return (
-        <section
+        <motion.section
             id="hero"
-            className="w-full flex flex-col-reverse sm:flex-row justify-center gap-10 md:gap-30 items-center p-5 md:p-10 my-16 animate-fade-in"
+            className="w-full flex flex-col-reverse sm:flex-row justify-center gap-10 md:gap-30 items-center p-00 md:p-10 my-12 sm:my-16"
+            initial="hidden"
+            animate="visible"
         >
             <div className="md:w-1/2">
-                <div className="flex flex-col justify-center gap-5 w-[270px] md:w-[431px]">
-                    <div className="animate-slide-up">
-                        <div className="flex flex-row">
-                            <h1 className="font-rubik uppercase font-medium text-primary text-3xl md:text-5xl transition duration-500 ease-in-out transform hover:scale-105">Hey, I{"'"}m</h1>
-                        </div>
-                        <div className="flex flex-row justify-end">
-                            <h1 className="font-rubik uppercase font-medium text-secondary text-3xl md:text-5xl transition duration-500 ease-in-out transform hover:scale-105">Ramez Ehab</h1>
-                        </div>
+                <motion.div
+                    className="flex flex-col justify-center gap-5 w-[270px] md:w-[325px] lg:w-[431px]"
+                    variants={containerVariants}
+                >
+                    <div>
+                        <motion.div className="flex flex-row"
+                            variants={itemVariants}
+                        >
+                            <motion.h1
+                                className="font-rubik uppercase font-medium text-primary text-3xl md:text-4xl lg:text-5xl"
+                                whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+                            >
+                                Hey, I{"'"}m
+                            </motion.h1>
+                        </motion.div>
+                        <motion.div
+                            className="flex flex-row justify-end"
+                            variants={sideSlideVariants}
+                        >
+                            <motion.h1
+                                className="font-rubik uppercase font-medium text-secondary text-3xl md:text-4xl lg:text-5xl"
+                                whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+                            >
+                                Ramez Ehab
+                            </motion.h1>
+                        </motion.div>
                     </div>
 
-                    <div className="animate-slide-up delay-150">
-                        <div className="flex flex-row">
-                            <h1 className="font-robotoMono font-medium text-primary text-lg md:text-2xl transition duration-500 ease-in-out transform hover:scale-105">I{"'"}m a</h1>
-                        </div>
-                        <div className="flex flex-row justify-end">
-                            <h1 className="font-robotoMono font-medium text-secondary text-lg md:text-2xl transition duration-500 ease-in-out transform hover:scale-105">Mobile | Web Developer</h1>
-                        </div>
+                    <div>
+                        <motion.div
+                            className="flex flex-row"
+                            variants={itemVariants}
+                        >
+                            <motion.h1
+                                className="font-robotoMono font-medium text-primary text-lg md:text-xl lg:text-2xl"
+                                whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+                            >
+                                I{"'"}m a
+                            </motion.h1>
+                        </motion.div>
+                        <motion.div
+                            className="flex flex-row justify-end"
+                            variants={itemVariants}
+                        >
+                            <motion.h1
+                                className="font-robotoMono font-medium text-secondary text-lg md:text-xl lg:text-2xl"
+                                whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+                            >
+                                Mobile | Web Developer
+                            </motion.h1>
+                        </motion.div>
                     </div>
 
-                    <div className="animate-slide-up delay-300">
-                        <div className="flex flex-row">
-                            <h1 className="font-robotoMono font-medium text-primary text-lg transition duration-500 ease-in-out transform hover:scale-105">Passionate about</h1>
-                        </div>
-                        <div className="flex flex-row justify-end">
-                            <h1 className="font-robotoMono font-medium text-secondary text-lg transition duration-500 ease-in-out transform hover:scale-105">Programming</h1>
-                        </div>
+                    <div>
+                        <motion.div
+                            className="flex flex-row"
+                            variants={itemVariants}
+                        >
+                            <motion.h1
+                                className="font-robotoMono font-medium text-primary text-lg"
+                                whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+                            >
+                                Passionate about
+                            </motion.h1>
+                        </motion.div>
+                        <motion.div
+                            className="flex flex-row justify-end"
+                            variants={itemVariants}
+                        >
+                            <motion.h1
+                                className="font-robotoMono font-medium text-secondary text-lg"
+                                whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+                            >
+                                Programming
+                            </motion.h1>
+                        </motion.div>
                     </div>
 
-                    <button
-                        className="bg-primary text-bgColor font-robotoMono font-medium text-base md:text-lg py-2 px-4 rounded-md w-fit transition duration-500 ease-in-out transform hover:scale-110 hover:bg-secondary"
+                    <motion.button
+                        className="bg-primary text-bgColor font-robotoMono font-medium text-base md:text-lg py-2 px-4 rounded-md w-fit mt-6 md:mt-0 mx-auto md:mx-0"
+                        variants={sideSlideVariants}
+                        whileHover={{ scale: 1.1, backgroundColor: '#51bfff' }}
+                        transition={{ duration: 0.3 }}
                     >
                         View my Resume
-                    </button>
-                </div>
-
+                    </motion.button>
+                </motion.div>
             </div>
 
-            <div className="w-3/4 xs:w-1/2 sm:w-1/3 md:w-1/2 lg:w-1/3 animate-slide-in">
-                <img
+            <motion.div
+                className="w-3/4 xs:w-1/2 sm:w-1/3"
+                initial="hidden"
+                animate="visible"
+                variants={imageVariants}
+            >
+                <motion.img
                     src="https://avatars.githubusercontent.com/u/117018553?v=4"
                     alt="Ramez Ehab"
-                    className="rounded-full transition duration-500 ease-in-out transform hover:scale-105"
+                    className="rounded-full"
+                    whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
                 />
-            </div>
-
-        </section>
-    )
-}
+            </motion.div>
+        </motion.section>
+    );
+};
 
 export default Hero;

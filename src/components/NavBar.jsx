328bc5ede88b6b435563dc2534b26867113ec45b
Ramez Ehab <ramezehab2@gmail.com>
2024-09-28 12:58:13 +0300

    chore: reformatting

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,12 @@
 const NavBar = () => {
-    return (
-      <div className="w-full flex flex-row md:gap-10 px-5 py-5 justify-around md:justify-end min-h-10 sticky top-0 bg-bgColor z-50">
-        <span className="font-robotoMono font-medium text-base md:text-lg text-secondary scale-110 transition transform hover:scale-110 cursor-pointer">Home</span>
-        <span className="font-robotoMono font-medium text-base md:text-lg text-primary transition transform hover:scale-110 cursor-pointer">About</span>
-        <span className="font-robotoMono font-medium text-base md:text-lg text-primary transition transform hover:scale-110 cursor-pointer">Projects</span>
-        <span className="font-robotoMono font-medium text-base md:text-lg text-primary transition transform hover:scale-110 cursor-pointer">Contact</span>
-      </div>
-    );
-  }
-  
-  export default NavBar;
-  
+  return (
+    <div className="w-full flex flex-row md:gap-10 px-5 py-5 justify-around lg:justify-end min-h-10 sticky top-0 bg-bgColor z-50">
+      <span className="font-robotoMono font-medium text-base md:text-lg text-secondary scale-110 transition transform hover:scale-110 cursor-pointer">Home</span>
+      <span className="font-robotoMono font-medium text-base md:text-lg text-primary transition transform hover:scale-110 cursor-pointer">About</span>
+      <span className="font-robotoMono font-medium text-base md:text-lg text-primary transition transform hover:scale-110 cursor-pointer">Projects</span>
+      <span className="font-robotoMono font-medium text-base md:text-lg text-primary transition transform hover:scale-110 cursor-pointer">Contact</span>
+    </div>
+  );
+}
+
+export default NavBar;

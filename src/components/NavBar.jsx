055fce66fead8317785e784a5c4758d062c7f496
Ramez Ehab <ramezehab2@gmail.com>
2024-09-30 23:24:39 +0300

    refactor: minor changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <div className="w-full flex flex-row md:gap-10 px-5 py-5 justify-around lg:justify-end min-h-10 sticky top-0 bg-black/65 z-50">
+    <div className="w-full flex flex-row md:gap-10 px-5 py-5 justify-around lg:justify-end min-h-10 sticky top-0 bg-black/65 z-10">
       <span className="font-robotoMono font-medium text-base md:text-lg text-secondary scale-110 transition transform hover:scale-110 cursor-pointer">Home</span>
       <span className="font-robotoMono font-medium text-base md:text-lg text-primary transition transform hover:scale-110 cursor-pointer">About</span>
       <span className="font-robotoMono font-medium text-base md:text-lg text-primary transition transform hover:scale-110 cursor-pointer">Projects</span>

0a5877e0947f8b469e1a69cea1399c0cced29da6
Ramez Ehab <ramezehab2@gmail.com>
2024-10-01 14:02:30 +0300

    feat: updating project pop up and project details

--- src/components/ProjectPopUp.jsx
+++ src/components/ProjectPopUp.jsx
@@ -13,32 +13,42 @@ const ProjectPopUp = () => {
             <div className="flex flex-col shadow-2xl rounded-lg min-h-[300px] max-h-[100%] w-[100%] h-[75%] sm:w-[85%] sm:h-[85%] bg-[#111] border-2 md:border-4 border-secondary p-4 space-y-5 overflow-y-auto">
                 <button
                     onClick={() => dispatch({ type: "TOGGLE_PROJECT_POPUP" })}
-                    className="bg-red-700 text-primary font-semibold font-rubik py-2 px-4 rounded-full w-min self-end hover:bg-red-800 transition-all"
+                    className="bg-red-700 text-primary text-xs md:text-base font-semibold font-rubik py-2 px-4 rounded-3xl w-min self-end hover:bg-red-800 transition-all"
                 >
                     X
                 </button>
 
                 <div className="flex flex-col justify-start items-center md:items-start h-full space-y-4 md:px-10 md:py-5">
                     <h2 className="text-3xl md:text-4xl font-rubik font-medium text-primary">{data.name}</h2>
-                    <p className="text-base md:text-lg text-center md:text-start text-primary/50">{data.long_description}</p>
-                    <div className="flex flex-col md:flex-row md:space-x-16 w-full h-full pb-2">
-                        <div className="flex flex-col justify-between md:max-w-[50%] h-full">
+                    <p className="text-sm sm:text-base md:text-lg text-center md:text-start text-primary/50">{data.long_description}</p>
+                    <div className="flex flex-col md:flex-row md:space-x-16 w-full h-full pb-2 ">
+                        <div className="flex flex-col justify-between md:max-w-[50%] h-full space-y-10">
 
                             <div className="flex flex-row flex-wrap justify-center md:justify-start items-start gap-x-2 gap-y-4 ">
                                 {data.tech_stack.map((tech, index) => (
-                                    <span key={index} className="bg-[#333] text-primary text-sm md:text-base font-rubik font-medium py-2 px-4 rounded-full h-fit w-fit hover:bg-[#444] transition-all cursor-pointer">
+                                    <span key={index} className="bg-[#333] text-primary text-sm md:text-base font-rubik font-medium py-2 px-2 md:px-4 rounded-2xl h-fit w-fit hover:bg-[#444] transition-all cursor-pointer">
                                         {tech}
                                     </span>
 
                                 ))}
                             </div>
 
-                            {/* {data.links.map((link, index) => (
-                                <a key={index} href={link.url} target="_blank" rel="noreferrer" className="bg-[#333] text-primary text-sm md:text-base font-rubik font-medium py-2 px-4 rounded-full w-min hover:bg-[#444] transition-all cursor-pointer">
-                                    {link.name}
-                                </a>
-                            ))} */}
+                            <div className="flex flex-row flex-wrap justify-center md:justify-start items-end gap-x-2 gap-y-4 sm:pb-4">
+                                {/* <span className="text-primary text-lg font-rubik font-medium text-center t">Links: </span> */}
+                                {data.links.map((link, index) => (
+                                    <a
+                                        key={index}
+                                        href={link.url}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        className="bg-[#333] p-2 rounded-2xl w-fit hover:bg-[#444] hover:scale-105 hover:shadow-lg transition-transform duration-200 cursor-pointer flex items-center space-x-3 "
+                                    >
+                                        <img src={link.img} alt={link.name} className="w-7 h-7" />
+                                        {/* <span>{link.name}</span> */}
+                                    </a>
+                                ))}
 
+                            </div>
                         </div>
 
                         <div className="max-w-0 md:min-w-[30%] md:max-w-[45%]">
--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,4 +1,5 @@
 import images from "./images";
+import icons from "./icons";
 
 const projects = [
   {
@@ -6,11 +7,17 @@ const projects = [
     name: "HST Risk",
     description: "React Native Expo",
     long_description: "A mobile app mimicking 'RISK' where teams battle for virtual countries using virtual money. Real-life challenges decide outcomes, and winners gain control of both their country and the opponent's.",
-    tech_stack: ["React Native", "Expo", "Node.js", "Express", "MongoDB"],
+    tech_stack: ["React Native", "Expo", "Tailwind CSS", "Node.js", "Express", "MongoDB", "Socket.io"],
     links: [
       {
         name: "GitHub",
-        url: ""
+        img: icons.github,
+        url: "", 
+      },
+      {
+        name: "Apple Store",
+        img: icons.apple, 
+        url: "https://apps.apple.com/us/app/hst-risk/id6636564359", 
       },
     ],
     category: "Mobile",

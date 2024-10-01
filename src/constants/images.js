a02103d2072cdf8d6c0a51c40193a0c0d91bd6cc
Ramez Ehab <ramezehab2@gmail.com>
2024-10-02 00:08:12 +0300

    chore: add character image

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,9 @@
 import HST_Risk from '../assets/images/HST_Risk.png';
+import character from '../assets/images/character.png';
 
 const images = {
     HST_Risk,
+    character,
 };
 
 export default images;

e323f87d2fb3783b254e40a590cdb56648af61f9
Ramez Ehab <ramezehab2@gmail.com>
2024-10-02 00:56:21 +0300

    chore: update icons & projects

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -2,12 +2,17 @@ import linkedin from '../assets/icons/linkedin.png';
 import github from '../assets/icons/github.png';
 import apple from '../assets/icons/apple.png';
 import website from '../assets/icons/website.png';
+import email from '../assets/icons/email.png';
+
+import HST_Risk from '../assets/icons/HST-Risk.png';
 
 const icons = {
     linkedin,
     github,
     apple,
     website,
+    email,
+    HST_Risk,
 };
 
 export default icons;
--- src/constants/projects.js
+++ src/constants/projects.js
@@ -4,6 +4,7 @@ import icons from "./icons";
 const projects = [
   {
     image: images.HST_Risk,
+    icon: icons.HST_Risk,
     name: "HST Risk",
     description: "React Native Expo",
     long_description: "A mobile app mimicking 'RISK' where teams battle for virtual countries using virtual money. Real-life challenges decide outcomes, and winners gain control of both their country and the opponent's.",
@@ -22,19 +23,6 @@ const projects = [
     ],
     category: "Mobile",
   },
-  // {
-  //   image: "https://via.placeholder.com/250",
-  //   title: "Project 2",
-  //   description: "A React Native project.",
-  //   tech_stack: ["React Native", "Expo", "Node.js", "Express", "MongoDB"],
-  //   links: [
-  //     {
-  //       name: "GitHub",
-  //       url: ""
-  //     },
-  //   ],
-  //   category: "Web",
-  // },
 ];
 
 

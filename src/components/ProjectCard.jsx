b8e326f053b5519074dc46e30737865202f1671c
Ramez Ehab <ramezehab2@gmail.com>
2025-04-04 14:41:17 +0200

    feat: add an extra project item with imgs and icon & format some code

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -27,13 +27,13 @@ const ProjectCard = ({ projectData }) => {
         <div
             className={`flip-card w-32 h-32 sm:w-48 sm:h-48 transition-transform duration-700 ease-out transform overflow-hidden`}
         >
-            <div className="flip-card-inner h-full w-full relative">
+            <div className="relative w-full h-full flip-card-inner">
                 {/* Front side */}
-                <div className="flip-card-front flex flex-col justify-between items-center h-full w-full absolute backface-hidden">
+                <div className="absolute flex flex-col justify-between items-center w-full h-full backface-hidden flip-card-front">
                     <img
                         src={projectData.icon}
                         alt={projectData.name}
-                        className="object-cover rounded-md mb-3 h-[50%] sm:h-[60%] w-auto"
+                        className="mb-3 rounded-md w-auto h-[50%] sm:h-[60%] object-contain"
                     />
                     <div className="flex flex-col justify-center items-center">
                         <span className="font-robotoMono font-medium text-primary text-base sm:text-lg text-center">
@@ -46,11 +46,11 @@ const ProjectCard = ({ projectData }) => {
                 </div>
 
                 {/* Back side */}
-                <div className="flip-card-back flex flex-col justify-evenly items-center rounded-md shadow-lg gap-y-4 h-full w-full absolute backface-hidden transform rotateY-180">
+                <div className="absolute flex flex-col justify-evenly items-center gap-y-4 shadow-lg rounded-md w-full h-full backface-hidden flip-card-back transform rotateY-180">
                     <h1 className="font-robotoMono font-medium text-primary text-base sm:text-lg text-center">{projectData.name}</h1>
                     <p className="font-robotoMono font-medium text-primary/50 text-xs sm:text-base text-center">{projectData.description}</p>
                     <button
-                        className="bg-primary text-bgColor font-robotoMono font-medium text-xs xs:text-base md:text-lg py-2 px-4 rounded-md w-fit transition duration-500 ease-in-out transform hover:scale-110 hover:bg-secondary"
+                        className="bg-primary hover:bg-secondary px-4 py-2 rounded-md w-fit font-robotoMono font-medium text-bgColor text-xs xs:text-base md:text-lg hover:scale-110 transition duration-500 ease-in-out transform"
                         onClick={() => {
                             if (!state.projectPopUpVisible)
                                 dispatch({ type: "TOGGLE_PROJECT_POPUP", payload: projectData });
